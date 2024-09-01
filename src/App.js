347262b5e745fe15c1a8541e58d9f0ee54021379
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-09-01 20:12:05 +0530

    new features add to app js file

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import TabButton from './components/tabButton/tabButton';
 import {useState} from 'react'
 import './App.css';
 
+
+
 function App() {
-  const[data,getData]=useState(0);
-  function calcy(data){
-    getData(data);
+  const[data,getData]=useState("0");
+  function number(datas){
+      if(data==="0"){
+         getData(datas);
+      }
+      else
+      getData(data+datas);
   }
+  
+  function calcy(operator){
+    let temp=number(data);
+
+  }
+    
   return (
     <div className="App">
-      <TabButton whenClick={()=>calcy(1)}>1</TabButton>
-      <TabButton whenClick={()=>calcy(2)}>2</TabButton>
-      <TabButton whenClick={()=>calcy(3)}>3</TabButton>
-      <TabButton whenClick={()=>calcy(4)}>4</TabButton>
-      <TabButton whenClick={()=>calcy(5)}>5</TabButton>
-      <TabButton whenClick={()=>calcy(6)}>6</TabButton>
-      <TabButton whenClick={()=>calcy(7)}>7</TabButton>
-      <TabButton whenClick={()=>calcy(8)}>8</TabButton>
-      <TabButton whenClick={()=>calcy(9)}>9</TabButton>
-      <TabButton whenClick={()=>calcy(0)}>0</TabButton>
+      <main>
+      <TabButton whenClick={()=>number("1")}>1</TabButton>
+      <TabButton whenClick={()=>number("2")}>2</TabButton>
+      <TabButton whenClick={()=>number("3")}>3</TabButton>
+      <TabButton whenClick={()=>number("4")}>4</TabButton>
+      <TabButton whenClick={()=>number("5")}>5</TabButton>
+      <TabButton whenClick={()=>number("6")}>6</TabButton>
+      <TabButton whenClick={()=>number("7")}>7</TabButton>
+      <TabButton whenClick={()=>number("8")}>8</TabButton>
+      <TabButton whenClick={()=>number("9")}>9</TabButton>
+      <TabButton whenClick={()=>number("0")}>0</TabButton>
+      <TabButton  whenClick={()=>calcy('add')}>+</TabButton>
+      <TabButton whenClick={()=>calcy('sub')}>-</TabButton>
+      <TabButton whenClick={()=>calcy('div')}>/</TabButton>
+      <TabButton whenClick={()=>calcy('mul')}>*</TabButton>
+      </main>
+
       <h1>{data}</h1>
     </div>
   );
