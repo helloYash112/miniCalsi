be880260df3d8f36c3bba2da287203476f2c9973
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-09-01 18:43:54 +0530

    appjs modefied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import TabButton from './components/tabButton/tabButton';
-import {usesatate} from 'React'
+import {useState} from 'react'
 import './App.css';
 
 function App() {
-  const[data,getData]=usesatate(0);
+  const[data,getData]=useState(0);
   function calcy(data){
     getData(data);
   }
   return (
     <div className="App">
       <TabButton whenClick={()=>calcy(1)}>1</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>2</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>3</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>4</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>5</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>6</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>7</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>8</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>9</TabButton>
-      <TabButton whenClick={()=>calcy(1)}>0</TabButton>
+      <TabButton whenClick={()=>calcy(2)}>2</TabButton>
+      <TabButton whenClick={()=>calcy(3)}>3</TabButton>
+      <TabButton whenClick={()=>calcy(4)}>4</TabButton>
+      <TabButton whenClick={()=>calcy(5)}>5</TabButton>
+      <TabButton whenClick={()=>calcy(6)}>6</TabButton>
+      <TabButton whenClick={()=>calcy(7)}>7</TabButton>
+      <TabButton whenClick={()=>calcy(8)}>8</TabButton>
+      <TabButton whenClick={()=>calcy(9)}>9</TabButton>
+      <TabButton whenClick={()=>calcy(0)}>0</TabButton>
       <h1>{data}</h1>
     </div>
   );
