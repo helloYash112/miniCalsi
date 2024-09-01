16f8a46045934381861bfb62ef979c38b5ab5fa1
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-09-01 18:27:24 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import TabButton from './components/tabButton/tabButton';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <TabButton>1</TabButton>
+      <TabButton>2</TabButton>
+      <TabButton>3</TabButton>
+      <TabButton>4</TabButton>
+      <TabButton>5</TabButton>
+      <TabButton>6</TabButton>
+      <TabButton>7</TabButton>
+      <TabButton>8</TabButton>
+      <TabButton>9</TabButton>
+      <TabButton>0</TabButton>
     </div>
   );
 }
---
+++ src/components/tabButton/tabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton({children,whenClick}){
+    return<>
+    <button onClick={whenClick}>{children}</button></>
+
+}
