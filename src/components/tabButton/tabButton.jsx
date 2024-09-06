1bf927fa8535bae10b1441f39cb6291d0a123aa4
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-09-06 22:53:52 +0530

    completed

--- src/components/tabButton/tabButton.jsx
+++ src/components/tabButton/tabButton.jsx
@@ -1,6 +1,6 @@
 import './tabButton.css';
 export default function TabButton({children,whenClick,name}){
     return<>
-    <button id='btn1' className={name} onClick={whenClick}>{children}</button></>
+    <button id={name ?undefined :'btn1'} className={name} onClick={whenClick}>{children}</button></>
 
 }
